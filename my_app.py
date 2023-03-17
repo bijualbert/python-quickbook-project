1d92f8f745088f49448eaa0c3a9c6cf14446112d
bijualbert <bijualbert@gmail.com>
2023-03-17 12:58:24 -0700

    Add files via upload

---
+++ my_app.py
@@ -0,0 +1,15 @@
+from docx import Document
+import pyttsx3
+
+def speak(text):
+    pyttsx3.speak('text000')
+
+document = Document()
+
+name = input('name ')
+phone_number = input('phone ')
+email = input('email ')
+
+document.add_paragraph(name + ' | ' + phone_number + ' | ' + email)
+
+document.save('cv.docx')
